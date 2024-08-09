71adbc0969b7c4773d159748e496e109abb8d296
Mandar Patil <mandarons@pm.me>
2024-08-08 23:05:13 -0700

    icloudpy upgraded to 0.6.0 and unraid template updated to set ENV_ICLOUD_PASSWORD as optional (#254)

--- tests/data/__init__.py
+++ tests/data/__init__.py
@@ -3886,7 +3886,7 @@ class ICloudPySessionMock(base.ICloudPySession):
         if "com.apple.photos.cloud" in url:
             if url.endswith("zones/list"):
                 return ResponseMock(ZONES_LIST_WORKING)
-            if "query?remapEnums=True&getCurrentSyncToken=True" in url:
+            if url.endswith("remapEnums=True&getCurrentSyncToken=True"):
                 if data.get("query").get("recordType") == "CheckIndexingState":
                     return ResponseMock(
                         photos_data.DATA[
